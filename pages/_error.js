5092fb53b0a477f085b9e6e617a83900816d272c
Leonardo <lfotecam@gmail.com>
2019-08-25 15:39:12 -0500

    refactor some pages

--- pages/_error.js
+++ pages/_error.js
@@ -14,33 +14,14 @@ class Error extends React.Component{
     render() {
         const { statusCode } = this.props
       return (
-          <Fragment>
-                {
-                    statusCode === 404 
-                    ? <div className="message">
-                        <h1>{this.props.t('title')}</h1>
-                        <Link href='/'>
-                            <a>
-                                <h3>Volver a la home! o que?</h3>
-                            </a>
-                        </Link>
-                    </div>
-                    : <div className="message">
-                    <h1>Ha ocurrido un error generico</h1>
-                    <Link href='/'>
-                            <a>
-                                <h3>Volver a la home!</h3>
-                            </a>
-                        </Link>
-                </div>
-                }
-                <style jsx>{`
-                    .message{
-                        padding: 100px;
-                        text-align: center;
-                    }
-                `}</style>
-            </Fragment>
+        <div className="message">
+            <h1>{this.props.t('title')}</h1>
+            <Link href='/'>
+                <a>
+                    <h3>{this.props.t('subtitle')}</h3>
+                </a>
+            </Link>
+        </div>
       )
     }
   }
