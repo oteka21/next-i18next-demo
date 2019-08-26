69df55c12d36c8c5b48480f014f95fa08fa27663
leonardo oteca <leonardo@sinchicreaciones.com>
2019-08-26 09:31:52 -0500

    last solution was not good

--- i18n.js
+++ i18n.js
@@ -1,23 +1,24 @@
 const NextI18Next = require('next-i18next').default
 
-// module.exports = new NextI18Next({
-//   defaultLanguage: 'de',
-//   otherLanguages: ['en'],
-//   browserLanguageDetection: true,
-// })
+module.exports = new NextI18Next({
+  defaultLanguage: 'de',
+  otherLanguages: ['en'],
+  fallbackLng: ['es','de'],
+  browserLanguageDetection: true,
+})
 
-const nextI18nOptions = {
-  defaultLanguage: 'en',
-  otherLanguages: ['de'],
-  browserLanguageDetection: false
-};
-const languages = nextI18nOptions.otherLanguages;
-languages.push(nextI18nOptions.defaultLanguage);
+// const nextI18nOptions = {
+//   defaultLanguage: 'en',
+//   otherLanguages: ['de'],
+//   browserLanguageDetection: false
+// };
+// const languages = nextI18nOptions.otherLanguages;
+// languages.push(nextI18nOptions.defaultLanguage);
 
-const NextI18NextInstance = new NextI18Next({ ...nextI18nOptions });
+// const NextI18NextInstance = new NextI18Next({ ...nextI18nOptions });
 
-if (NextI18NextInstance && NextI18NextInstance.i18n && !NextI18NextInstance.i18n.languages) {
-  NextI18NextInstance.i18n.languages = languages;
-}
+// if (NextI18NextInstance && NextI18NextInstance.i18n && !NextI18NextInstance.i18n.languages) {
+//   NextI18NextInstance.i18n.languages = languages;
+// }
 
-module.exports = NextI18NextInstance;
+// module.exports = NextI18NextInstance;
